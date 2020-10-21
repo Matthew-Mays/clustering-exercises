65cdc8ee66d60ae481271b849ad89911bef66251
Matthew-Mays <matthew.c.mays96@gmail.com>
2020-10-21 00:46:42 -0500

    added a couple more functions to acquire.py

--- acquire.py
+++ acquire.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from env import user, password, host
+import os
 
 def get_connection(db, user=user, host=host, password=password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
@@ -33,3 +34,26 @@ def cols_missing(df):
     pct_cols_missing = df.isnull().sum(axis=1)/df.shape[1]*100
     missing_cols_df = pd.DataFrame({'num_cols_missing': num_cols_missing, 'pct_cols_missing': pct_cols_missing}).reset_index().groupby(['num_cols_missing','pct_cols_missing']).count().rename(index=str, columns={'index': 'num_rows'}).reset_index()
     return missing_cols_df
+
+
+def get_iris_data():
+    filename = 'iris.csv'
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        df = pd.read_sql('SELECT * FROM measurements JOIN species USING(species_id)', get_connection('iris_db'))
+        df.to_csv(filename, index = False)
+        return df
+
+def new_mall_data():
+    sql_query = 'SELECT * FROM customers'
+    df = pd.read_sql(sql_query, get_connection('mall_customers'))
+    df.to_csv('mall_customers_df.csv')
+    return df
+
+def get_mall_data(cached=False):
+    if cached or os.path.isfile('mall_customers_df.csv') == False:
+        df = new_mall_data()
+    else:
+        df = pd.read_csv('mall_customers_df.csv', index_col = 0)
+    return df
